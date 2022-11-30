f78440c50411dc1ea7fe380361d62f34d0489008
roii31-game <74428415+roii31-game@users.noreply.github.com>
2022-11-29 23:33:14 -0600

    Pizza-Menu

---
+++ src/Components/Apps/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Business from "../Business/Business";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Business />
+    </div>
+  );
+}
+
+export default App;
